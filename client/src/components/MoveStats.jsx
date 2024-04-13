4f3a506615d4873defb1a44d983443db5f72ecea
Julian Mena Viñas <julianmenav2@gmail.com>
2024-04-13 17:44:09 +0200

    feat: make inputs numeric

--- client/src/components/MoveStats.jsx
+++ client/src/components/MoveStats.jsx
@@ -13,10 +13,11 @@ export default function MoveStats({handleOnChange, moveName, armorClass, setArmo
         <div className="flex-1 flex flex-col justify-between gap-2 p-3 rounded-md bg-cyan-600/30">
           <label htmlFor="caInput">CA Enemigo:</label>
           <input
+            inputMode="numeric"
             id="caInput"
             className=" rounded-sm w-full flex flex-center text-center "
             maxLength={2}
-            value={armorClass}
+            value={!isNaN(armorClass) ? armorClass : ""}
             placeholder={0}
             onChange={(e) => setArmorClass(e.target.value)}
           />
@@ -24,10 +25,11 @@ export default function MoveStats({handleOnChange, moveName, armorClass, setArmo
         <div className="flex-1 flex flex-col justify-between gap-2 p-3 rounded-md bg-cyan-600/30">
           <label htmlFor="bonusAttackInput">Bon. Ataque:</label>
           <input
+            inputMode="numeric"
             id="bonusAttackInput"
             className=" rounded-sm w-full flex flex-center text-center"
             maxLength={2}
-            value={bonusAttack}
+            value={!isNaN(bonusAttack) ? bonusAttack : ""}
             placeholder={0}
             onChange={(e) => setBonusAttack(e.target.value)}
           />
@@ -35,10 +37,11 @@ export default function MoveStats({handleOnChange, moveName, armorClass, setArmo
         <div className="flex-1 flex flex-col justify-between gap-2 p-3 rounded-md bg-cyan-600/30">
           <label htmlFor="bonusDamageInput">Bon. Daño:</label>
           <input
+            inputMode="numeric"
             id="bonusDamageInput"
             className=" rounded-sm w-full flex flex-center text-center"
             maxLength={2}
-            value={bonusDamage}
+            value={!isNaN(bonusDamage) ? bonusDamage : ""}
             placeholder={0}
             onChange={(e) => setBonusDamage(e.target.value)}
           />
--- client/src/hooks/useStats.js
+++ client/src/hooks/useStats.js
@@ -7,10 +7,14 @@ export default function useAttacks({initialArmorClass, initialBonusAttack, initi
   const [armorClass, setArmorClass] = useState(initialArmorClass);      // CA
   const [bonusAttack, setBonusAttack] = useState(initialBonusAttack)     // Bonificador
   const [bonusDamage, setBonusDamage] = useState(initialBonusDamage);    // Bonificador DMG
+
+  const numberArmorClass = Number(armorClass);
+  const numberBonusAttack = Number(bonusAttack);
+  const numberBonusDamage = Number(bonusDamage);
   
   const [moveName, setMoveName] = useState("");
   const handleOnChange = (event) => setMoveName(event.target.value);
 
 
-  return {handleOnChange, moveName, armorClass, setArmorClass, bonusAttack, setBonusAttack, bonusDamage, setBonusDamage};
+  return {handleOnChange, moveName, armorClass: numberArmorClass, setArmorClass, bonusAttack: numberBonusAttack, setBonusAttack, bonusDamage:numberBonusDamage, setBonusDamage};
 }
