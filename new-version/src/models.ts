1f25a2126104e0d59c8950fb3dba2af0c0a73ff2
Juli <julianmenav2@gmail.com>
2025-06-17 23:38:32 +0200

    style: prettierize everything

--- new-version/src/models.ts
+++ new-version/src/models.ts
@@ -1,33 +1,39 @@
-export type Dice = 4 | 6 | 8 | 10 | 12 | 20;
+export type Dice = 4 | 6 | 8 | 10 | 12 | 20
 
-export type AbilityType = 'Strength' | 'Dexterity' | 'Constitution' | 'Intelligence' | 'Wisdom' | 'Charisma';
+export type AbilityType =
+  | 'Strength'
+  | 'Dexterity'
+  | 'Constitution'
+  | 'Intelligence'
+  | 'Wisdom'
+  | 'Charisma'
 
 export type Attack = {
-  id: string;
-  dices: Dice[];
-  feats: string[];
-  attackBonusAbility?: AbilityType;
-  damageBonusAbility?: AbilityType;
-  parentTurnId: string;
-  parentCharacterId: string;
-};
+  id: string
+  dices: Dice[]
+  feats: string[]
+  attackBonusAbility?: AbilityType
+  damageBonusAbility?: AbilityType
+  parentTurnId: string
+  parentCharacterId: string
+}
 
 export type Turn = {
-  id: string;
-  name: string;
-  enemyAc?: number;
-  attacks: Attack[];
-};
+  id: string
+  name: string
+  enemyAc?: number
+  attacks: Attack[]
+}
 
 export type Character = {
-  id: string;
-  name: string;
-  lvl: number;
-  abilities: Record<AbilityType, number>; // from -5 to +5
-  turns: Turn[];
-};
+  id: string
+  name: string
+  lvl: number
+  abilities: Record<AbilityType, number> // from -5 to +5
+  turns: Turn[]
+}
 
 export type Scenario = {
-  enemyAc: number;
-  characters: Character[];
-};
+  enemyAc: number
+  characters: Character[]
+}
--- new-version/src/store/scenarioStore.ts
+++ new-version/src/store/scenarioStore.ts
@@ -1,221 +1,269 @@
-
-import { persist } from 'zustand/middleware';
-import type { Scenario, Character, Turn, Attack } from '../models';
-import { v4 as uuidv4 } from 'uuid';
-import { create } from 'zustand';
-
+import { persist } from 'zustand/middleware'
+import type { Scenario, Character, Turn, Attack } from '../models'
+import { v4 as uuidv4 } from 'uuid'
+import { create } from 'zustand'
 
 interface ScenarioState {
-    scenario: Scenario;
-    actions: {
-        updateEnemyAc: (newAc: number) => void;
+  scenario: Scenario
+  actions: {
+    updateEnemyAc: (newAc: number) => void
 
-        addCharacter: (character?: Character) => void;
-        addTurn: (characterId: string, turn?: Turn) => void;
-        addAttack: (characterId: string, turnId: string, attack?: Attack) => void;
+    addCharacter: (character?: Character) => void
+    addTurn: (characterId: string, turn?: Turn) => void
+    addAttack: (characterId: string, turnId: string, attack?: Attack) => void
 
-        updateCharacter: (characterId: string, updatedCharacter: Partial<Character>) => void;
-        updateTurn: (characterId: string, turnId: string, updatedTurn: Partial<Turn>) => void;
-        updateAttack: (characterId: string, turnId: string, attackId: string, updatedAttack: Partial<Attack>) => void;
+    updateCharacter: (
+      characterId: string,
+      updatedCharacter: Partial<Character>
+    ) => void
+    updateTurn: (
+      characterId: string,
+      turnId: string,
+      updatedTurn: Partial<Turn>
+    ) => void
+    updateAttack: (
+      characterId: string,
+      turnId: string,
+      attackId: string,
+      updatedAttack: Partial<Attack>
+    ) => void
 
-        removeCharacter?: (characterId: string) => void;
-        removeTurn?: (characterId: string, turnId: string) => void;
-        removeAttack?: (characterId: string, turnId: string, attackId: string) => void;
-    }
+    removeCharacter: (characterId: string) => void
+    removeTurn: (characterId: string, turnId: string) => void
+    removeAttack: (
+      characterId: string,
+      turnId: string,
+      attackId: string
+    ) => void
+  }
 }
 
 export const useScenarioStore = create<ScenarioState>()(
-    persist(
-        (set) => ({
+  persist(
+    (set) => ({
+      scenario: {
+        enemyAc: 10,
+        characters: [],
+      },
+      actions: {
+        updateEnemyAc: (newAc: number) =>
+          set((state) => ({
             scenario: {
-                enemyAc: 10,
-                characters: [],
+              ...state.scenario,
+              enemyAc: newAc,
             },
-            actions: {
-                updateEnemyAc: (newAc: number) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        enemyAc: newAc,
-                    }
-                })),
-                
-                addCharacter: (character?: Character) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        characters: [...state.scenario.characters, createCharacter(character)],
-                    }
-                })),
+          })),
 
-                addTurn: (characterId: string, turn?: Turn) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        characters: state.scenario.characters.map(character =>
-                            character.id === characterId
-                                ? {...character, turns: [...character.turns, createTurn(turn)]}
-                                : character
-                        ),
-                    }
-                })),
+        addCharacter: (character?: Character) =>
+          set((state) => ({
+            scenario: {
+              ...state.scenario,
+              characters: [
+                ...state.scenario.characters,
+                createCharacter(character),
+              ],
+            },
+          })),
 
-                addAttack: (characterId: string, turnId: string, attack?: Attack) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        characters: state.scenario.characters.map(character =>
-                            character.id === characterId
-                                ? {
-                                    ...character,
-                                    turns: character.turns.map(turn =>
-                                        turn.id === turnId
-                                            ? {...turn, attacks: [...turn.attacks, createAttack(attack)]}
-                                            : turn
-                                    )
-                                }
-                                : character
-                        ),
+        addTurn: (characterId: string, turn?: Turn) =>
+          set((state) => ({
+            scenario: {
+              ...state.scenario,
+              characters: state.scenario.characters.map((character) =>
+                character.id === characterId
+                  ? {
+                      ...character,
+                      turns: [...character.turns, createTurn(turn)],
                     }
-                })),
+                  : character
+              ),
+            },
+          })),
 
-                updateCharacter: (characterId: string, updatedCharacter: Partial<Character>) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        characters: state.scenario.characters.map(character =>
-                            character.id === characterId
-                                ? {...character, ...updatedCharacter}
-                                : character
-                        ),
+        addAttack: (characterId: string, turnId: string, attack?: Attack) =>
+          set((state) => ({
+            scenario: {
+              ...state.scenario,
+              characters: state.scenario.characters.map((character) =>
+                character.id === characterId
+                  ? {
+                      ...character,
+                      turns: character.turns.map((turn) =>
+                        turn.id === turnId
+                          ? {
+                              ...turn,
+                              attacks: [...turn.attacks, createAttack(attack)],
+                            }
+                          : turn
+                      ),
                     }
-                })),
+                  : character
+              ),
+            },
+          })),
 
-                updateTurn: (characterId: string, turnId: string, updatedTurn: Partial<Turn>) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        characters: state.scenario.characters.map(character =>
-                            character.id === characterId
-                                ? {
-                                    ...character,
-                                    turns: character.turns.map(turn =>
-                                        turn.id === turnId
-                                            ? {...turn, ...updatedTurn}
-                                            : turn
-                                    )
-                                }
-                                : character
-                        ),
-                    }
-                })),
+        updateCharacter: (
+          characterId: string,
+          updatedCharacter: Partial<Character>
+        ) =>
+          set((state) => ({
+            scenario: {
+              ...state.scenario,
+              characters: state.scenario.characters.map((character) =>
+                character.id === characterId
+                  ? { ...character, ...updatedCharacter }
+                  : character
+              ),
+            },
+          })),
 
-                updateAttack: (characterId: string, turnId: string, attackId: string, updatedAttack: Partial<Attack>) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        characters: state.scenario.characters.map(character =>
-                            character.id === characterId
-                                ? {
-                                    ...character,
-                                    turns: character.turns.map(turn =>
-                                        turn.id === turnId
-                                            ? {
-                                                ...turn,
-                                                attacks: turn.attacks.map(attack =>
-                                                    attack.id === attackId
-                                                        ? {...attack, ...updatedAttack}
-                                                        : attack
-                                                )
-                                            }
-                                            : turn
-                                    )
-                                }
-                                : character
-                        ),
+        updateTurn: (
+          characterId: string,
+          turnId: string,
+          updatedTurn: Partial<Turn>
+        ) =>
+          set((state) => ({
+            scenario: {
+              ...state.scenario,
+              characters: state.scenario.characters.map((character) =>
+                character.id === characterId
+                  ? {
+                      ...character,
+                      turns: character.turns.map((turn) =>
+                        turn.id === turnId ? { ...turn, ...updatedTurn } : turn
+                      ),
                     }
-                })),
+                  : character
+              ),
+            },
+          })),
 
-                removeCharacter: (characterId: string) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        characters: state.scenario.characters.filter(character => character.id !== characterId),
+        updateAttack: (
+          characterId: string,
+          turnId: string,
+          attackId: string,
+          updatedAttack: Partial<Attack>
+        ) =>
+          set((state) => ({
+            scenario: {
+              ...state.scenario,
+              characters: state.scenario.characters.map((character) =>
+                character.id === characterId
+                  ? {
+                      ...character,
+                      turns: character.turns.map((turn) =>
+                        turn.id === turnId
+                          ? {
+                              ...turn,
+                              attacks: turn.attacks.map((attack) =>
+                                attack.id === attackId
+                                  ? { ...attack, ...updatedAttack }
+                                  : attack
+                              ),
+                            }
+                          : turn
+                      ),
                     }
-                })),
-                removeTurn: (characterId: string, turnId: string) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        characters: state.scenario.characters.map(character =>
-                            character.id === characterId
-                                ? {
-                                    ...character,
-                                    turns: character.turns.filter(turn => turn.id !== turnId)
-                                }
-                                : character
-                        ),
-                    }
-                })),
+                  : character
+              ),
+            },
+          })),
 
-                removeAttack: (characterId: string, turnId: string, attackId: string) => set((state) => ({
-                    scenario: {
-                        ...state.scenario,
-                        characters: state.scenario.characters.map(character =>
-                            character.id === characterId
-                                ? {
-                                    ...character,
-                                    turns: character.turns.map(turn =>
-                                        turn.id === turnId
-                                            ? {
-                                                ...turn,
-                                                attacks: turn.attacks.filter(attack => attack.id !== attackId)
-                                            }
-                                            : turn
-                                    )
-                                }
-                                : character
-                        ),
+        removeCharacter: (characterId: string) =>
+          set((state) => ({
+            scenario: {
+              ...state.scenario,
+              characters: state.scenario.characters.filter(
+                (character) => character.id !== characterId
+              ),
+            },
+          })),
+        removeTurn: (characterId: string, turnId: string) =>
+          set((state) => ({
+            scenario: {
+              ...state.scenario,
+              characters: state.scenario.characters.map((character) =>
+                character.id === characterId
+                  ? {
+                      ...character,
+                      turns: character.turns.filter(
+                        (turn) => turn.id !== turnId
+                      ),
                     }
-                })),
-            }
-        }),
-        {
-            name: 'dnd-scenario-storage',
-            partialize: (state) => ({scenario: state.scenario}),
-        }
-    )
+                  : character
+              ),
+            },
+          })),
+
+        removeAttack: (characterId: string, turnId: string, attackId: string) =>
+          set((state) => ({
+            scenario: {
+              ...state.scenario,
+              characters: state.scenario.characters.map((character) =>
+                character.id === characterId
+                  ? {
+                      ...character,
+                      turns: character.turns.map((turn) =>
+                        turn.id === turnId
+                          ? {
+                              ...turn,
+                              attacks: turn.attacks.filter(
+                                (attack) => attack.id !== attackId
+                              ),
+                            }
+                          : turn
+                      ),
+                    }
+                  : character
+              ),
+            },
+          })),
+      },
+    }),
+    {
+      name: 'dnd-scenario-storage',
+      partialize: (state) => ({ scenario: state.scenario }),
+    }
+  )
 )
-
 
 // I think I will create new objects empty most of the time
 // but just in case I put an optional field to "clone" then if needed.
 
 const createCharacter = (character?: Character): Character => {
-    return {
-        id: uuidv4(),
-        name: character?.name || '',
-        lvl: character?.lvl || 1,
-        abilities: {
-            Strength: character?.abilities?.Strength || 0,
-            Dexterity: character?.abilities?.Dexterity || 0,
-            Constitution: character?.abilities?.Constitution || 0,
-            Intelligence: character?.abilities?.Intelligence || 0,
-            Wisdom: character?.abilities?.Wisdom || 0,
-            Charisma: character?.abilities?.Charisma || 0,
-        },
-        turns: character?.turns || [],
-    };
-};
+  return {
+    id: uuidv4(),
+    name: character?.name || '',
+    lvl: character?.lvl || 1,
+    abilities: {
+      Strength: character?.abilities?.Strength || 0,
+      Dexterity: character?.abilities?.Dexterity || 0,
+      Constitution: character?.abilities?.Constitution || 0,
+      Intelligence: character?.abilities?.Intelligence || 0,
+      Wisdom: character?.abilities?.Wisdom || 0,
+      Charisma: character?.abilities?.Charisma || 0,
+    },
+    turns: character?.turns || [],
+  }
+}
 
 const createTurn = (turn?: Turn): Turn => {
-    return {
-        id: uuidv4(),
-        name: '',
-        enemyAc: turn?.enemyAc,
-        attacks: [],
-    };
+  return {
+    id: uuidv4(),
+    name: '',
+    enemyAc: turn?.enemyAc,
+    attacks: [],
+  }
 }
 
 const createAttack = (attack?: Attack): Attack => {
-    return {
-        id: uuidv4(),
-        dices: attack?.dices || [],
-        feats: attack?.feats || [],
-        attackBonusAbility: attack?.attackBonusAbility,
-        damageBonusAbility: attack?.damageBonusAbility,
-        parentTurnId: attack?.parentTurnId || '',
-        parentCharacterId: attack?.parentCharacterId || '',
-    };
-};
+  return {
+    id: uuidv4(),
+    dices: attack?.dices || [],
+    feats: attack?.feats || [],
+    attackBonusAbility: attack?.attackBonusAbility,
+    damageBonusAbility: attack?.damageBonusAbility,
+    parentTurnId: attack?.parentTurnId || '',
+    parentCharacterId: attack?.parentCharacterId || '',
+  }
+}
