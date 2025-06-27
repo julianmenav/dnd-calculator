b4153b1266c42e68e7da916ae25f6adbe3e0f6be
Juli <julianmenav2@gmail.com>
2025-06-27 16:21:46 +0200

    feat: who the fuck told me to put d20 in damage rolls??

--- src/components/DiceChooser.tsx
+++ src/components/DiceChooser.tsx
@@ -7,6 +7,7 @@ import { useTurn } from '../context/TurnContext'
 import Plus from '../icons/Plus'
 import DiceSelection from './DiceSelection'
 import Confirm from '../icons/Confirm'
+import X from '../icons/X'
 
 export default function DiceChooser() {
   const [showDiceChooser, setShowDiceChooser] = useState(false)
@@ -25,33 +26,46 @@ export default function DiceChooser() {
           <Plus /> Add Attack
         </button>
       ) : (
-        <div>
-          <div className="flex flex-row gap-0.5 rounded-md bg-black/20 p-1">
-            {DICE_SIDES.map((dice) => (
+        <div className="w-full">
+          <div className="flex w-full flex-row justify-between gap-0.5 rounded-md bg-black/20 p-1">
+            <div className="flex flex-row items-center gap-0.5">
+              {DICE_SIDES.filter((dice) => dice !== 20).map((dice) => (
+                <button
+                  key={dice}
+                  className={`btn btn-xs btn-secondary text-base ${DICE_COLORS[dice].background} ${DICE_COLORS[dice].text} hover:opacity-70`}
+                  onClick={() => {
+                    if (dices.length > 20) return
+                    setDices((prev) => {
+                      const newDices = [...prev, dice]
+                      return newDices
+                    })
+                  }}
+                >
+                  {dice}
+                </button>
+              ))}
+            </div>
+            <div className="flex flex-row items-center gap-0.5">
               <button
-                key={dice}
-                className={`btn btn-xs btn-secondary text-base ${DICE_COLORS[dice].background} ${DICE_COLORS[dice].text} hover:opacity-70`}
+                className="btn-success btn btn-xs flex cursor-pointer items-center justify-center rounded-sm p-1 text-xs"
                 onClick={() => {
-                  if (dices.length > 20) return
-                  setDices((prev) => {
-                    const newDices = [...prev, dice]
-                    return newDices
-                  })
+                  addAttack(character.id, turn.id, dices)
+                  setDices([])
+                  setShowDiceChooser(false)
                 }}
               >
-                {dice}
+                <Confirm />
               </button>
-            ))}
-            <button
-              className="btn-success btn btn-xs flex cursor-pointer items-center justify-center rounded-sm p-1 text-xs"
-              onClick={() => {
-                addAttack(character.id, turn.id, dices)
-                setDices([])
-                setShowDiceChooser(false)
-              }}
-            >
-              <Confirm />
-            </button>
+              <button
+                className="btn-error btn btn-xs flex cursor-pointer items-center justify-center rounded-sm p-1 text-xs"
+                onClick={() => {
+                  setDices([])
+                  setShowDiceChooser(false)
+                }}
+              >
+                <X />
+              </button>
+            </div>
           </div>
           <DiceSelection dices={dices} />
         </div>
